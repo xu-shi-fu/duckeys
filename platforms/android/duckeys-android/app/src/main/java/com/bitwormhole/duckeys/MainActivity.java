afa571e03f0144cb45932ec981c635c30197abfb
MEZ <mez@bitwormhole.com>
2024-09-08 13:50:17 +0800

    补充了一些 android 项目的文件

---
+++ platforms/android/duckeys-android/app/src/main/java/com/bitwormhole/duckeys/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bitwormhole.duckeys;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
